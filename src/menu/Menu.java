46392840f812d3431432b40ef7b1bf3052a1392f
MisterSuav3 <202311582@fit.edu.ph>
2024-11-28 17:09:26 +0800

    menu

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -15,7 +15,38 @@ package menu;
  */
 
 public class Menu {
-    public Object[][] table = new String [3][];
+    public void menuOptions(){
+        System.out.println("++================================================================++");
+        System.out.println("++================================================================++");
+        System.out.println("||                                                                ||");
+        System.out.println("||            CCCCCCCCC          AA          RRRRRRRRRR           ||");
+        System.out.println("||           CCCCCCCCCC         AAAA         RRRRRRRRRRR          ||");
+        System.out.println("||          CCCC               AA  AA        RRR       RR         ||");
+        System.out.println("||          CCC               AA    AA       RRR       RR         ||");
+        System.out.println("||          CCC              AAAAAAAAAA      RRRRRRRRRRR          ||");
+        System.out.println("||          CCC             AAAAAAAAAAAA     RRRRRRRRRR           ||");
+        System.out.println("||          CCCC           AAAA      AAAA    RRR    RRRR          ||");
+        System.out.println("||           CCCCCCCCCC   AAAA        AAAA   RRR      RRRR        ||");
+        System.out.println("||             CCCCCCCC  AAAA          AAAA  RRR      RRRR        ||");
+        System.out.println("||                                                                ||");
+        System.out.println("||   RRRRRRRRRR     EEEEEEEEEE  NNNNN       NNN  TTTTTTTTTTTTTT   ||");
+        System.out.println("||   RRRRRRRRRRR    EEEEEEEEEE  NNNNNN      NNN  TTTTTTTTTTTTTT   ||");
+        System.out.println("||   RRR       RR   EEE         NNNNNNN     NNN       TTTT        ||");
+        System.out.println("||   RRR       RR   EEE         NNN  NNN    NNN       TTTT        ||");
+        System.out.println("||   RRRRRRRRRRR    EEEEEEEE    NNN   NNN   NNN       TTTT        ||");
+        System.out.println("||   RRRRRRRRRR     EEEEEEEE    NNN    NNNN NNN       TTTT        ||");
+        System.out.println("||   RRR    RRRR    EEE         NNN     NNNNNNN       TTTT        ||");
+        System.out.println("||   RRR      RRRR  EEEEEEEEEE  NNN       NNNNN       TTTT        ||");
+        System.out.println("||   RRR      RRRR  EEEEEEEEEE  NNN        NNNN       TTTT        ||");
+        System.out.println("||                                                                ||");
+        System.out.println("++================================================================++");
+        System.out.println("++================================================================++");
+
+        System.out.println("++================================================================++");
+        System.out.println("++================================================================++");
+
+    }
+
     public void display(){
 
     }
