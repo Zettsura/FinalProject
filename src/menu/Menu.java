42792d536febb3060e11ab48b73795a7b0e0cbfa
MisterSuav3 <202311582@fit.edu.ph>
2024-11-28 17:48:24 +0800

    Menu

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,4 +1,6 @@
 package menu;
+import vehicle.*;
+import java.util.Scanner;
 
 /**
  * TODO
@@ -15,39 +17,70 @@ package menu;
  */
 
 public class Menu {
-    public void menuOptions(){
-        System.out.println("++================================================================++");
-        System.out.println("++================================================================++");
-        System.out.println("||                                                                ||");
-        System.out.println("||            CCCCCCCCC          AA          RRRRRRRRRR           ||");
-        System.out.println("||           CCCCCCCCCC         AAAA         RRRRRRRRRRR          ||");
-        System.out.println("||          CCCC               AA  AA        RRR       RR         ||");
-        System.out.println("||          CCC               AA    AA       RRR       RR         ||");
-        System.out.println("||          CCC              AAAAAAAAAA      RRRRRRRRRRR          ||");
-        System.out.println("||          CCC             AAAAAAAAAAAA     RRRRRRRRRR           ||");
-        System.out.println("||          CCCC           AAAA      AAAA    RRR    RRRR          ||");
-        System.out.println("||           CCCCCCCCCC   AAAA        AAAA   RRR      RRRR        ||");
-        System.out.println("||             CCCCCCCC  AAAA          AAAA  RRR      RRRR        ||");
-        System.out.println("||                                                                ||");
-        System.out.println("||   RRRRRRRRRR     EEEEEEEEEE  NNNNN       NNN  TTTTTTTTTTTTTT   ||");
-        System.out.println("||   RRRRRRRRRRR    EEEEEEEEEE  NNNNNN      NNN  TTTTTTTTTTTTTT   ||");
-        System.out.println("||   RRR       RR   EEE         NNNNNNN     NNN       TTTT        ||");
-        System.out.println("||   RRR       RR   EEE         NNN  NNN    NNN       TTTT        ||");
-        System.out.println("||   RRRRRRRRRRR    EEEEEEEE    NNN   NNN   NNN       TTTT        ||");
-        System.out.println("||   RRRRRRRRRR     EEEEEEEE    NNN    NNNN NNN       TTTT        ||");
-        System.out.println("||   RRR    RRRR    EEE         NNN     NNNNNNN       TTTT        ||");
-        System.out.println("||   RRR      RRRR  EEEEEEEEEE  NNN       NNNNN       TTTT        ||");
-        System.out.println("||   RRR      RRRR  EEEEEEEEEE  NNN        NNNN       TTTT        ||");
-        System.out.println("||                                                                ||");
-        System.out.println("++================================================================++");
-        System.out.println("++================================================================++");
-
-        System.out.println("++================================================================++");
-        System.out.println("++================================================================++");
-
+public class Menu extends Vehicle {
+    public Scanner inp = new Scanner(System.in);
+    public void loginMenu(){
+        while(true) {
+            System.out.println("    CAR RENTAL  ");
+            System.out.println("+---------------+");
+            System.out.println("| [A] Sign-In   |");
+            System.out.println("| [B] Sign-Up   |");
+            System.out.println("| [X]  Exit     |");
+            System.out.println("+---------------+");
+            System.out.print("ENTER:");
+            String opt = inp.nextLine();
+            switch(opt) {
+                case "A": case "a":
+                    break;
+                case "B": case "b":
+                    break;
+                case "X": case "x":
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid Input");
+            }
+        }
     }
 
-    public void display(){
+    public void menuOptions(){
+        System.out.println("++=================================================================================++");
+        System.out.println("++=================================================================================++");
+        System.out.println("||   //                                                                            ||");
+        System.out.println("||  //                 CCCCCCCCC          AA          RRRRRRRRRR                   ||");
+        System.out.println("|| //                 CCCCCCCCCC         AAAA         RRRRRRRRRRR                  ||");
+        System.out.println("||//                 CCCC               AA  AA        RRR       RR                 ||");
+        System.out.println("||/                  CCC               AA    AA       RRR       RR                 ||");
+        System.out.println("||                   CCC              AAAAAAAAAA      RRRRRRRRRRR                  ||");
+        System.out.println("||                   CCC             AAAAAAAAAAAA     RRRRRRRRRR                   ||");
+        System.out.println("||                   CCCC           AAAA      AAAA    RRR    RRRR                  ||");
+        System.out.println("||                    CCCCCCCCCC   AAAA        AAAA   RRR      RRRR                ||");
+        System.out.println("||                      CCCCCCCC  AAAA          AAAA  RRR      RRRR                ||");
+        System.out.println("||                                                                                 ||");
+        System.out.println("||            RRRRRRRRRR     EEEEEEEEEE  NNNNN       NNN  TTTTTTTTTTTTTT           ||");
+        System.out.println("||            RRRRRRRRRRR    EEEEEEEEEE  NNNNNN      NNN  TTTTTTTTTTTTTT           ||");
+        System.out.println("||            RRR       RR   EEE         NNNNNNN     NNN       TTTT                ||");
+        System.out.println("||            RRR       RR   EEE         NNN  NNN    NNN       TTTT                ||");
+        System.out.println("||            RRRRRRRRRRR    EEEEEEEE    NNN   NNN   NNN       TTTT                ||");
+        System.out.println("||            RRRRRRRRRR     EEEEEEEE    NNN    NNNN NNN       TTTT                ||");
+        System.out.println("||            RRR    RRRR    EEE         NNN     NNNNNNN       TTTT                ||");
+        System.out.println("||            RRR      RRRR  EEEEEEEEEE  NNN       NNNNN       TTTT                ||");
+        System.out.println("||            RRR      RRRR  EEEEEEEEEE  NNN        NNNN       TTTT                ||");
+        System.out.println("||                                                                                 ||");
+        System.out.println("++=================================================================================++");
+        System.out.println("++=================================================================================++");
+        while(true) {
+            System.out.println("++==============================================++");
+            System.out.println("++==============================================++");
+            System.out.println("|| [A]         Display All Vehicles             ||");
+            System.out.println("|| [B]               Search                     ||");
+            System.out.println("|| [C]                                          ||");
+            System.out.println("|| [D]              Log Out                     ||");
+            System.out.println("++==============================================++");
+            System.out.println("++==============================================++");
+        }
+    }
+
+    public void displayAll(){
 
     }
 
