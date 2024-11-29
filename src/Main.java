7b3eca4b1fa2f617d6a8036eab13995c1f71650f
Adrian Christian Santos <adrian.crsantos@gmail.com>
2024-11-29 21:08:01 +0800

    change

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main (String[] args) {
+        // Actual
         VehicleFileHandler vfh = new VehicleFileHandler();
         UserFileHandler ufh = new UserFileHandler();
+        Authentication auth = new Authentication();
 
-        /// FOR TESTING
+        // FOR TESTING
         Vehicle sedan1 = new Vehicle(102412412L, "Sedan", "Suzuki", "1.3-CLE-CVT", "Red", "Diesel", "Manual", 5, 523.42, 2000.1, true, false, 200.1, 0.0, 500, 300, false);
         Vehicle sedan2 = new Vehicle(102423415L, "Sedan", "Tonda", "2025-Tivic-Sport-Sedan", "Black", "Unleaded", "Automatic", 5, 643.73, 1234.1, false, true, 0.0, 0.0, 10, 400, false);
         Vehicle sedan3 = new Vehicle(102473345L, "Sedan", "Pooschie", "Panamera", "Blue", "Diesel", "Manual", 5, 895.152, 5000.1, false, true, 0.0, 0.0, 20, 200, true);
@@ -22,17 +24,14 @@ public class Main {
         vehicles.add(sedan3);
 
         VehicleFileHandler.save(vehicles);
-        List<Vehicle> filtered = Search.filterByColor(vfh.load(), "Yellow");
+        List<Vehicle> filtered = Search.filterByColor(vfh.load(), "Red");
         for (Vehicle vehicle : filtered) {
             System.out.println(vehicle.getCarBrand() + " Color: " + vehicle.getColor());
         }
 
         User user = new User(12L, "Adrian", "adrian@gmail.com", "1234", "Customer", 12L);
-        Authentication auth = new Authentication();
         auth.register(user);
         auth.login("adrian@gmail.com", "1234");
         System.out.println(Authentication.getAuthenticatedUser().getName());
-
-
     }
 }
