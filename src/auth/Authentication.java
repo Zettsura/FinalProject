ab8f698453353ed9a7533553e314a604fc296b51
Adrian Christian Santos <adrian.crsantos@gmail.com>
2024-11-29 10:15:34 +0800

    define Authentication and User classes

--- src/auth/Authentication.java
+++ src/auth/Authentication.java
@@ -1,4 +1,101 @@
 package auth;
 
+import file.UserFileHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+// TODO: Handle exceptions
 public class Authentication {
+    public static User getAuthenticatedUser() {
+        return authenticatedUser;
+    }
+
+    public static void setAuthenticatedUser(User authenticatedUser) {
+        Authentication.authenticatedUser = authenticatedUser;
+    }
+
+    static private User authenticatedUser;
+    private final ArrayList<User> userList;
+    private HashMap<String, User> userHashMap;
+
+    public Authentication() {
+        UserFileHandler ufh = new UserFileHandler();
+        userList = ufh.load();
+        populateMap();
+
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
+        while (true) {
+
+            // TODO: Prompt for login/registration
+            System.out.println("1: login, 2: register: ");
+            try {
+                choice = sc.nextInt();
+                switch (choice) {
+                    case 1 -> loginPrompt(sc);
+                    case 2 -> registerPrompt(sc);
+                    default -> throw new RuntimeException();
+                }
+            } catch (RuntimeException ex) {
+                // TODO: Handle exception
+            }
+        }
+    }
+
+    public Authentication(ArrayList<User> userList) {
+        this.userList = userList;
+        populateMap();
+    }
+
+    private void populateMap() {
+        userHashMap = new HashMap<>();
+        for (User user : userList) {
+            userHashMap.put(user.getEmail(), user);
+        }
+    }
+
+    // TODO: Create prompt
+    public void loginPrompt(Scanner sc) {
+        String email = sc.nextLine();
+        String password = sc.nextLine();
+
+        login(email, password);
+    }
+
+    // TODO: Create prompt
+    public void registerPrompt(Scanner sc) {
+        User newUser = new User();
+        newUser.setEmail(sc.nextLine());
+        newUser.setPassword(sc.nextLine());
+        newUser.setName(sc.nextLine());
+
+        newUser.setUserType("Customer");
+        newUser.setUserId(1);           // TODO: Create ID generator
+        register(newUser);
+    }
+
+    // TODO: Handle exceptions
+    public int login(String email, String password) {
+        int status = -1;
+        email = email.trim();
+        password = password.trim();
+        User user = userHashMap.get(email);
+        if (user.getPassword().equals(password)) {
+            status = 0;
+            setAuthenticatedUser(user);
+        }
+        return status;
+    }
+
+    // TODO: Handle exceptions
+    public void register(User user) {
+        userList.add(user);
+        userHashMap.put(user.getEmail(), user);
+        UserFileHandler.save(userList);
+    };
 }
+
+
+
--- src/auth/User.java
+++ src/auth/User.java
@@ -1,4 +1,59 @@
 package auth;
 
 public class User {
+    private long userId;
+    private String name;
+    private String email;
+    private String password;
+    private String userType;
+
+    public User(){}
+    public User(long userId, String name, String email, String password, String userType){
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
 }
