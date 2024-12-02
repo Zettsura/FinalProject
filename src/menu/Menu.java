ade1c5403d2fe1022afcbe1ff8343fcdd4619229
MisterSuav3 <202311582@fit.edu.ph>
2024-12-02 12:08:03 +0800

    MenuOpts++++++++++++++++++++++++++++

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,6 +1,5 @@
 package menu;
 import file.VehicleFileHandler;
-import file.UserFileHandler;
 import vehicle.*;
 import rent.Rent;
 import java.util.List;
@@ -11,26 +10,25 @@ import java.util.Scanner;
 
 public class Menu {
     public Scanner inp = new Scanner(System.in);
+    public String opt;
     private Authentication auth;
     public Rent rentVehicles;
-    public String opt;
-    public User user;
 
     public Menu(List<Vehicle> vehicles, Authentication auth){
         this.rentVehicles = new Rent(vehicles);
         this.auth = auth;
     }
 
-    private String vehicleId = "Vehicle Id", carModel = "Model", color = "Color", fuelType = "Fuel Type", isAutomatic = "Transmission Type", modelId = "Model Id", passLim = "Passenger Limit", mileageLim = "Mileage Limit", canOffRoad = "Off Road Capability";
-    protected Vehicle sedan1 = new Vehicle(695783304L , "Sedan", "Suzuki", "1.3-CLE-CVT", "Red", "Diesel", "Manual", 5, 50.0, 1500.1, false, false, 200, 0, 500, 300, false);
-    protected Vehicle sedan2 = new Vehicle(102423415L, "Sedan", "Tonda", "2025-Tivic-Sport-Sedan", "Black", "Unleaded", "Automatic", 5, 50.0, 2400.1, false, true, 700, 0, 750, 200, false);
-    protected Vehicle sedan3 = new Vehicle(528692830L, "Sedan", "Pooschie", "Panamera", "Blue", "Diesel", "Manual", 5, 50.0, 2000.1, false, false, 500, 0, 450, 150, false);
-    protected Vehicle van1 = new Vehicle(229680265L, "Van", "Tonda", "2024-Tonda-ODYSSEY", "Pink", "Unleaded", "Manual", 8, 50.0, 6500.69, false, true, 3700, 0, 1700, 350, true);
-    protected Vehicle van2 = new Vehicle(232352365L, "Van", "Kyundai", "US4", "Green", "Diesel", "Manual", 8, 50.0, 4500.69, false, false, 3200, 0, 1500, 400, true);
+    private String vehicleId = "Vehicle Id", carType = "Type", brand = "Brand", color = "Color", fuelType = "Fuel Type", isAutomatic = "Transmission Type", modelId = "Model Id", passLim = "Passenger Limit", mileageLim = "Mileage Limit", canOffRoad = "Off Road Capability", towingCap = "Towing Capacity", truckBed = "Truck Bed Capacity", torque = "Torque", storageLim = "Storage Limit", extraSeats = "Extra Seats";
+    protected Vehicle sedan1 = new Vehicle(695783304L , "Sedan", "Suzuki", "1.3-CLE-CVT", "Red", "Diesel", "Manual", 5, 50.0, 2000.69, false, false, 200, 0, 500, 300, false);
+    protected Vehicle sedan2 = new Vehicle(102423415L, "Sedan", "Tonda", "2025-Tivic-Sport-Sedan", "Black", "Unleaded", "Automatic", 5, 50.0, 2000.69, false, true, 700, 0, 750, 200, false);
+    protected Vehicle sedan3 = new Vehicle(528692830L, "Sedan", "Pooschie", "Panamera", "Blue", "Diesel", "Manual", 5, 50.0, 2000.69, false, false, 500, 0, 450, 150, false);
+    protected Vehicle van1 = new Vehicle(229680265L, "Van", "Tonda", "2024-Tonda-ODYSSEY", "Pink", "Unleaded", "Manual", 8, 50.0, 5000.69, false, true, 3700, 0, 1700, 350, true);
+    protected Vehicle van2 = new Vehicle(232352365L, "Van", "Kyundai", "US4", "Green", "Diesel", "Manual", 8, 50.0, 5000.69, false, false, 3200, 0, 1500, 400, true);
     protected Vehicle van3 = new Vehicle(433252976L, "Van", "Yoyota", "B721", "White", "Premium", "Automatic", 8, 50.0, 5000.69, false, false, 3500, 0, 1450, 400, true);
     protected Vehicle pickUp1 = new Vehicle(242097450L, "Pick-Up", "Tord", "F150", "Gray", "Unleaded", "Manual", 2, 50.0, 10000.69, false, true, 7000, 100, 2300, 600, false);
-    protected Vehicle pickUp2 = new Vehicle(128952996L, "Pick-Up", "Kia", "F160", "Black", "Unleaded", "Manual", 2, 50.0, 11000.69, false, true, 6800, 300, 2400, 800, false);
-    protected Vehicle pickUp3 = new Vehicle(684529782L, "Pick-Up", "Toyota", "G987", "White", "Premium", "Manual", 2, 50.0, 12500.69, false, true, 7500, 500, 2500, 1000, false);
+    protected Vehicle pickUp2 = new Vehicle(128952996L, "Pick-Up", "Kia", "F160", "Black", "Unleaded", "Manual", 2, 50.0, 10000.69, false, true, 6800, 300, 2400, 800, false);
+    protected Vehicle pickUp3 = new Vehicle(684529782L, "Pick-Up", "Toyota", "G987", "White", "Premium", "Manual", 2, 50.0, 10000.69, false, true, 7500, 500, 2500, 1000, false);
 
     public void initializeVehicles(List<Vehicle> vehicles){
         if (vehicles.isEmpty()){
@@ -49,35 +47,35 @@ public class Menu {
     }
 
     public void loginMenu(){
-        while(true) {
+        do{
             System.out.println(" CAR RENTAL Log-In");
             System.out.println("+-----------------+");
             System.out.println("| [1] Sign-In     |");
             System.out.println("| [2] Register    |");
             System.out.println("| [3] Exit        |");
             System.out.println("+-----------------+");
-            System.out.print("ENTER: ");
+            System.out.print("Enter: ");
             try {
-                int opt = inp.nextInt();
+                String opt = inp.next();
                 switch (opt) {
-                    case 1:
+                    case "1":
                         auth.loginPrompt(inp);
+                        System.out.println("Welcome " + Authentication.getAuthenticatedUser().getName() + "!");
                         menuOptions();
-                    case 2:
+                    case "2":
                         auth.registerPrompt(inp);
                         continue;
-                    case 3: System.exit(0);
+                    case "3": System.exit(0);
                     default: throw new RuntimeException("Invalid");
                 }
             } catch (AuthenticationException ex) {
                 System.out.println("ERROR: " + ex.getMessage());
             } catch (InputMismatchException ex){
-                inp.nextLine();
                 System.out.println("ERROR: " + ex.getMessage());
             } catch (RuntimeException ex) {
                 System.out.println("ERROR: " + ex.getMessage());
             }
-        }
+        }while(true);
     }
 
 
@@ -132,11 +130,11 @@ public class Menu {
                         opt = inp.next();
                         if(Objects.equals(opt, "Y") || Objects.equals(opt, "y")) {
                             run = false;
-                            System.out.println("=======================================");
+                            System.out.println("=======================================\n");
                             break;
                         }
                         else if(Objects.equals(opt, "N") || Objects.equals(opt, "n")) {
-                            System.out.println("=======================================");
+                            System.out.println("=======================================\n");
                             continue;
                         }
                     case "E": case "e":
@@ -146,18 +144,20 @@ public class Menu {
                         if(Objects.equals(opt, "Y") || Objects.equals(opt, "y")) {
                             auth.delete(Authentication.getAuthenticatedUser());
                             run = false;
-                            System.out.println("=======================================");
+                            System.out.println("=======================================\n");
                             break;
                         }else if(Objects.equals(opt, "N") || Objects.equals(opt, "n")) {
-                            System.out.println("=======================================");
+                            System.out.println("=======================================\n");
                             continue;
                         }
                     default:
                         throw new RuntimeException("Invalid");
                 }
-            }catch(InputMismatchException ex){
+            }catch (InputMismatchException ex){
+                System.out.println("=======================================");
                 System.out.println("ERROR: " + ex.getMessage());
-            }catch (RuntimeException ex){
+            } catch (RuntimeException ex){
+                System.out.println("=======================================");
                 System.out.println("ERROR: " + ex.getMessage());
             }
         } while (run);
@@ -202,13 +202,7 @@ public class Menu {
 
     public void rentACar(){
         Scanner input = new Scanner(System.in);
-        UserFileHandler userFileHandler = new UserFileHandler();
         List<Vehicle> availableVehicles = rentVehicles.getRentedVehicleList();
-
-        if(user.getVehicleId() != 0){
-            System.out.println("You can only rent one vehicle at a time");
-            return;
-        }
 
         if (availableVehicles.isEmpty()) {
             System.out.println("===================================================");
@@ -219,64 +213,54 @@ public class Menu {
 
         displayCars();
         try {
-            System.out.println("==============================================================");
-            System.out.println(" Enter the number of the vehicle that you would like to rent: ");
+            System.out.printf("Enter the number of the vehicle that you would like to rent: ");
             int choice = input.nextInt();
 
-        if(choice >= 1 && choice <= availableVehicles.size()){
-            Vehicle vehicleChoice = availableVehicles.get(choice-1);
-            long vehicleId = vehicleChoice.getVehicleId();
+            if ((choice - 1) >= 0 && choice < availableVehicles.size()) {
+                Vehicle vehicleChoice = availableVehicles.get(choice - 1);
+                long vehicleId = vehicleChoice.getVehicleId();
 
-            rentVehicles.rentCar(vehicleId);
-            rentVehicles.updateRentedVehicleList();
-            user.setVehicleId(vehicleId);
-            VehicleFileHandler.save(rentVehicles.getRentedVehicleList());
+                rentVehicles.rentCar(vehicleId);
 
-            List<User> userList = userFileHandler.load();
+                rentVehicles.updateRentedVehicleList();
 
-            for (User u : userList){
-                if (u.getUserId() == user.getUserId()){
-                    u.setVehicleId(vehicleId);
-                    break;
-                }
-            }
-            UserFileHandler.save(userList);
+                VehicleFileHandler.save(rentVehicles.getRentedVehicleList());
+                System.out.print("========================================================================");
+                System.out.println("\nThe vehicle that you rented is");
+                System.out.format("%-25s%s%n", this.carType, vehicleChoice.getCarType());
+                System.out.format("%-25s%s%n", this.vehicleId, vehicleChoice.getVehicleId());
+                System.out.format("%-25s%s%n", this.brand, vehicleChoice.getCarBrand());
+                System.out.format("%-25s%s%n", this.modelId, vehicleChoice.getModelId());
+                System.out.format("%-25s%s%n", this.color, vehicleChoice.getColor());
+                System.out.format("%-25s%s%n", this.fuelType, vehicleChoice.getFuelType());
+                System.out.format("%-25s%s%n", this.isAutomatic, vehicleChoice.getTransmissionType());
+                System.out.format("%-25s%s%n", this.passLim, vehicleChoice.getPassLim());
+                System.out.format("%-25s%s%n", this.mileageLim, vehicleChoice.getMileageLim());
+                System.out.format("%-25s%s%n", this.canOffRoad, vehicleChoice.isCanOffRoad() ? "Capable" : "Incapable");
+                System.out.format("%-25s%s%n", this.towingCap, vehicleChoice.getTowingCap() + " kg");
+                System.out.format("%-25s%s%n", this.truckBed, vehicleChoice.getTruckBedCap() + " kg");
+                System.out.format("%-25s%s%n", this.truckBed, vehicleChoice.getTorque() + " Nm");
+                System.out.format("%-25s%s%n", this.storageLim, vehicleChoice.getStorageLim() + " kg");
+                System.out.format("%-25s%s%n", this.extraSeats, vehicleChoice.isHasExtraSeats() ? "Available" : "Unavailable" );
 
-            System.out.println("\nThe vehicle that you rented is");
-            System.out.format("%-15s%-10s%-10s%-15s%-25s%-25s%-20s%-15s%-15s%-15s%-15s%-15s%-15s%-15s%-15s%n",
-                    vehicleChoice.getCarType(),
-                    vehicleChoice.getVehicleId(),
-                    vehicleChoice.getCarBrand(),
-                    vehicleChoice.getModelId(),
-                    vehicleChoice.getColor(),
-                    vehicleChoice.getFuelType(),
-                    vehicleChoice.getTransmissionType(),
-                    vehicleChoice.getPassLim(),
-                    vehicleChoice.getMileageLim(),
-                    vehicleChoice.isCanOffRoad(),
-                    vehicleChoice.getTowingCap(),
-                    vehicleChoice.getTruckBedCap(),
-                    vehicleChoice.getTorque(),
-                    vehicleChoice.getStorageLim(),
-                    vehicleChoice.isHasExtraSeats());
-        }else {throw new RuntimeException("Invalid");}
+            }else {throw new RuntimeException("Invalid");}
         }catch (InputMismatchException ex){
+            System.out.println("=======================================");
             System.out.println("ERROR: " + ex.getMessage());
         } catch (RuntimeException ex){
+            System.out.println("=======================================");
             System.out.println("ERROR: " + ex.getMessage());
         }
-
-        System.out.print("========================================================================");
-        System.out.print("========================================================================");
         System.out.println("========================================================================");
     }
 
     public void printType(String type){
         List<Vehicle> availableVehicles = rentVehicles.getRentedVehicleList();
+        int i = 1;
         printColumn();
         for (Vehicle v : availableVehicles) {
             if (Objects.equals(v.getCarType(), type)) {
-                System.out.format("%-10s%-15s%-15s%-25s%-10s%-15s%-15s%-10s%-15s%-15s%-15s%-5s%-5s%-5s%n",
+                System.out.format("%-5s%-10s%-15s%-15s%-25s%-10s%-15s%-15s%-10s%-15s%-15s%-15s%-15s%-15s%-15s%-10s%n",i ,
                         v.getCarType(),
                         v.getVehicleId(),
                         v.getCarBrand(),
@@ -285,50 +269,50 @@ public class Menu {
                         v.getFuelType(),
                         v.getTransmissionType(),
                         v.getPassLim(),
-                        v.getMileageLim(),
-                        v.isCanOffRoad(),
-                        v.getTowingCap(),
-                        v.getTruckBedCap(),
-                        v.getTorque(),
-                        v.getStorageLim(),
-                        v.isHasExtraSeats());
+                        v.getMileageLim() + " mi",
+                        v.isCanOffRoad() ? "Capable" : "Incapable",
+                        v.getTowingCap() + " kg",
+                        v.getTruckBedCap() + " kg",
+                        v.getTorque() + " Nm",
+                        v.getStorageLim() + " kg",
+                        v.isHasExtraSeats() ? "Yes" : "No");
+                i++;
             }
         }
     }
 
     public void filterCars(){
         List<Vehicle> availableVehicles = rentVehicles.getRentedVehicleList();
-            System.out.println("========================================");
-            System.out.println("Which type of car would you like to see:\n\t[1] Sedan\n\t[2] Van\n\t[3] Pick-up Truck");
-            System.out.println("========================================");
-            System.out.print("Enter: ");
-            Scanner input = new Scanner(System.in);
-            try {
-                int choice = input.nextInt();
-                switch (choice) {
-                    case 1:
-                        printType("Sedan");
-                        System.out.println();
-                        break;
-                    case 2:
-                        printType("Van");
-                        System.out.println();
-                        break;
-                    case 3:
-                        printType("Pick-Up");
-                        System.out.println();
-                        break;
-                    default:
-                        throw new RuntimeException("Invalid");
-                }
-            } catch (InputMismatchException ex) {
-                System.out.println("ERROR: " + ex.getMessage());
-            } catch (RuntimeException ex){
-                System.out.println("ERROR: " + ex.getMessage());
+        System.out.println("       ========================================");
+        System.out.println("       Which type of car would you like to see:\n\t   [1] Sedan\n\t   [2] Van\n\t   [3] Pick-up Truck");
+        System.out.println("       ========================================");
+        System.out.print("       Enter: ");
+        Scanner input = new Scanner(System.in);
+        try {
+            String choice = input.next();
+            switch (choice) {
+                case "1":
+                    printType("Sedan");
+                    break;
+                case "2":
+                    printType("Van");
+                    break;
+                case "3":
+                    printType("Pick-Up");
+                    break;
+                default:
+                    throw new RuntimeException("Invalid");
             }
-            System.out.print("========================================================================");
-            System.out.print("========================================================================");
-            System.out.println("========================================================================");
+        } catch (InputMismatchException ex){
+            System.out.println("=======================================");
+            System.out.println("ERROR: " + ex.getMessage());
+        } catch (RuntimeException ex){
+            System.out.println("=======================================");
+            System.out.println("ERROR: " + ex.getMessage());
+        }
+        System.out.print("========================================================================");
+        System.out.print("========================================================================");
+        System.out.println("========================================================================");
     }
 
     public void printColumn(){
@@ -391,5 +375,3 @@ public class Menu {
         System.out.println("========================================================================");
     }
 }
-
-
